f24a56b5ed25135248a44ac57f28207fb78ae529
Cristina Pichiu <cristinapichiu38@gmail.com>
2024-02-25 23:27:30 +0200

    auth

--- security/src/main/java/com/cristina/security/config/JwtService.java
+++ security/src/main/java/com/cristina/security/config/JwtService.java
@@ -5,13 +5,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -19,7 +20,8 @@ public class JwtService {
     private static final String SECRET_KEY = "";
     
     public String extractUsername(String token) {
-        return null;
+
+        return  extractClaim(token, Claims::getSubject);
     }
 
     public String generateToken(UserDetails userDetails)
@@ -40,6 +42,25 @@ public class JwtService {
                 .signWith(getSignInKey(), SignatureAlgorithm.ES256)
                 .compact();
     }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim( String token, Function<Claims, T> claimsResover){
+        final Claims claims = extractAllClaims(token);
+        return claimsResover.apply(claims);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
